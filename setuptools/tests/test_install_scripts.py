6921a8d93ca6bf9f5bf8d94f66474ab88ba5f801
Felix Krull <f_krull@gmx.de>
2016-06-25 21:20:27 +0200

    Add test to verify shebang generation on non-Windows systems.
    Check that even when supplied a --executable argument containing spaces,
    non-Windows systems should never escape paths in shebangs since they can't
    deal with it anyway and it prevents use cases as described in issue #188.

--- setuptools/tests/test_install_scripts.py
+++ setuptools/tests/test_install_scripts.py
@@ -11,31 +11,61 @@ from setuptools.dist import Distribution
 from . import contexts
 
 
-@pytest.yield_fixture
-def sys_executable_with_space(monkeypatch):
-    e = 'C:\\Program Files\\Python\\python.exe'
-    monkeypatch.setattr('sys.executable', e)
-    yield e
-
-
 class TestInstallScripts:
-    @pytest.mark.skipif(sys.platform != 'win32',
-                        reason='Only makes sense on Windows')
-    def test_sys_executable_with_space(self, tmpdir, sys_executable_with_space):
-        settings = dict(
-            name='foo',
-            entry_points={'console_scripts': ['foo=foo:foo']},
-            version='0.0',
-        )
-        with tmpdir.as_cwd():
-            dist = Distribution(settings)
-            dist.script_name = 'setup.py'
-            cmd = install_scripts(dist)
-            cmd.install_dir = str(tmpdir)
-            cmd.ensure_finalized()
-            with contexts.quiet():
-                cmd.run()
+    settings = dict(
+        name='foo',
+        entry_points={'console_scripts': ['foo=foo:foo']},
+        version='0.0',
+    )
+    unix_exe = '/usr/bin/env python'
+    win32_exe = 'C:\\Program Files\\Python 3.3\\python.exe'
 
+    def _run_install_scripts(self, install_dir, executable=None):
+        dist = Distribution(self.settings)
+        dist.script_name = 'setup.py'
+        cmd = install_scripts(dist)
+        cmd.install_dir = install_dir
+        if executable is not None:
+            bs = cmd.get_finalized_command('build_scripts')
+            bs.executable = executable
+        cmd.ensure_finalized()
+        with contexts.quiet():
+            cmd.run()
+
+    @pytest.mark.skipif(sys.platform == 'win32', reason='non-Windows only')
+    def test_sys_executable_escaping_unix(self, tmpdir, monkeypatch):
+        monkeypatch.setattr('sys.executable', self.unix_exe)
+        expected = '#!%s\n' % self.unix_exe
+        with tmpdir.as_cwd():
+            self._run_install_scripts(str(tmpdir))
+            with io.open(str(tmpdir.join('foo')), 'r') as f:
+                actual = f.readline()
+        assert actual == expected
+
+    @pytest.mark.skipif(sys.platform != 'win32', reason='Windows only')
+    def test_sys_executable_escaping_win32(self, tmpdir, monkeypatch):
+        expected = '#!"%s"\n' % self.win32_exe
+        monkeypatch.setattr('sys.executable', self.win32_exe)
+        with tmpdir.as_cwd():
+            self._run_install_scripts(str(tmpdir))
             with io.open(str(tmpdir.join('foo-script.py')), 'r') as f:
-                shebang = f.readline()
-        assert shebang == '#!"%s"\n' % sys_executable_with_space
+                actual = f.readline()
+        assert actual == expected
+
+    @pytest.mark.skipif(sys.platform == 'win32', reason='non-Windows only')
+    def test_executable_escaping_unix(self, tmpdir):
+        expected = '#!%s\n' % self.unix_exe
+        with tmpdir.as_cwd():
+            self._run_install_scripts(str(tmpdir), self.unix_exe)
+            with io.open(str(tmpdir.join('foo')), 'r') as f:
+                actual = f.readline()
+        assert actual == expected
+
+    @pytest.mark.skipif(sys.platform != 'win32', reason='Windows only')
+    def test_executable_escaping_win32(self, tmpdir):
+        expected = '#!"%s"\n' % self.win32_exe
+        with tmpdir.as_cwd():
+            self._run_install_scripts(str(tmpdir), self.win32_exe)
+            with io.open(str(tmpdir.join('foo-script.py')), 'r') as f:
+                actual = f.readline()
+        assert actual == expected
