7493bd4bc530e03ea9b84f026788e10ef489cccf
Felix Krull <f_krull@gmx.de>
2016-06-25 21:20:27 +0200

    Add test to verify shebang generation on non-Windows systems.
    Check that even when supplied a --executable argument containing spaces,
    non-Windows systems should never escape paths in shebangs since they can't
    deal with it anyway and it prevents use cases as described in issue #188.

--- setuptools/tests/test_install_scripts.py
+++ setuptools/tests/test_install_scripts.py
@@ -11,17 +11,10 @@ from setuptools.dist import Distribution
 from . import contexts
 
 
-@pytest.yield_fixture
-def sys_executable_with_space(monkeypatch):
-    e = 'C:\\Program Files\\Python\\python.exe'
-    monkeypatch.setattr('sys.executable', e)
-    yield e
-
-
 class TestInstallScripts:
-    @pytest.mark.skipif(sys.platform != 'win32',
-                        reason='Only makes sense on Windows')
-    def test_sys_executable_with_space(self, tmpdir, sys_executable_with_space):
+    @pytest.mark.skipif(sys.platform != 'win32', reason='Windows only')
+    def test_executable_escaping_win32(self, tmpdir):
+        executable = 'C:\\Program Files\\Python\\python.exe'
         settings = dict(
             name='foo',
             entry_points={'console_scripts': ['foo=foo:foo']},
@@ -32,10 +25,35 @@ class TestInstallScripts:
             dist.script_name = 'setup.py'
             cmd = install_scripts(dist)
             cmd.install_dir = str(tmpdir)
+            bs = cmd.get_finalized_command('build_scripts')
+            bs.executable = executable
             cmd.ensure_finalized()
             with contexts.quiet():
                 cmd.run()
 
             with io.open(str(tmpdir.join('foo-script.py')), 'r') as f:
                 shebang = f.readline()
-        assert shebang == '#!"%s"\n' % sys_executable_with_space
+        assert shebang == '#!"%s"\n' % executable
+
+    @pytest.mark.skipif(sys.platform == 'win32', reason='non-Windows only')
+    def test_executable_escaping_non_win32(self, tmpdir):
+        executable = '/usr/bin/env python'
+        settings = dict(
+            name='foo',
+            entry_points={'console_scripts': ['foo=foo:foo']},
+            version='0.0',
+        )
+        with tmpdir.as_cwd():
+            dist = Distribution(settings)
+            dist.script_name = 'setup.py'
+            cmd = install_scripts(dist)
+            cmd.install_dir = str(tmpdir)
+            bs = cmd.get_finalized_command('build_scripts')
+            bs.executable = executable
+            cmd.ensure_finalized()
+            with contexts.quiet():
+                cmd.run()
+
+            with io.open(str(tmpdir.join('foo')), 'r') as f:
+                shebang = f.readline()
+        assert shebang == '#!%s\n' % executable
